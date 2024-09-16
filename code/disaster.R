9a6df8da7be4c2cdae5004df7bc2880f3567b751
Danny Del Rosso <delrossod79@gmail.com>
2024-09-16 12:15:17 -0400

    here goes nothing

--- code/disaster.R
+++ code/disaster.R
@@ -24,6 +24,13 @@ disaster <- read.csv(here('original', 'disaster.csv'), header = TRUE)
 View(disaster)
 
 disaster_filtered <- filter(disaster, Year %in% 2000:2019, Disaster.Type %in% c('Earthquake', 'Drought'))
-disaster_filtered_select <- dplyr::select(disaster_filtered, c('Year', 'ISO', 'Disaster.Type'))
+disaster_filtered_select <- dplyr::select(disaster_filtered, c('Year', 'ISO', 'Disaster.Type')) %>%
+  mutate(drought = if_else(Disaster.Type == 'Drought', 1, 0)) %>%
+  mutate(earthquake = if_else(Disaster.Type == 'Earthquake', 1, 0))
 
-View(disaster_filtered_select)
+disaster_filtered_select <- disaster_filtered_select %>% group_by(Year, ISO) %>%
+  summarise(drought = max(drought), earthquake = max(earthquake))
+
+
+
+
